76cf77915ab09b276b5f38d9256083d0f87ee4ac
jh-0130 <yongjh-jm22@student.tarc.edu.my>
2025-03-29 23:44:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pickle
 import numpy as np
 
+import os
+
+print("Files in directory:", os.listdir())
+
 with open("classifier.pkl", "rb") as model_file:
     model = pickle.load(model_file)
 
